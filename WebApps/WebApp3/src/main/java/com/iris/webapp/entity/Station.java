2de25f6efc0ed42a7d24173b240ff53c9ebb95bf
suyash <suyash@192.168.1.10>
2018-11-04 01:27:05 +0530

    metro app: first check in

---
+++ WebApps/WebApp3/src/main/java/com/iris/webapp/entity/Station.java
@@ -0,0 +1,41 @@
+package com.iris.webapp.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "station")
+public class Station implements Serializable {
+
+	private static final long serialVersionUID = -1000119078147252957L;
+
+	@Id
+	@Column(name = "id", length = 11, nullable = false)
+	private int id;
+
+	@Column(name = "name", length = 255, nullable = false)
+	private String name;
+
+	public Station() {
+	}
+
+	public int getCode() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
