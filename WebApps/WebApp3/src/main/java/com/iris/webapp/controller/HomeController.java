6048ec09c3a72eaaebd3f8482944deb9e36b125e
suyash <suyash@192.168.1.10>
2018-11-20 23:11:47 +0530

    metro: introduced station id

--- WebApps/WebApp3/src/main/java/com/iris/webapp/controller/HomeController.java
+++ WebApps/WebApp3/src/main/java/com/iris/webapp/controller/HomeController.java
@@ -3,25 +3,36 @@ package com.iris.webapp.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.iris.webapp.dao.RestaurantDAO;
 import com.iris.webapp.dao.StationDAO;
+import com.iris.webapp.entity.Restaurant;
 import com.iris.webapp.entity.Station;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class HomeController {
 
 	@Autowired
 	private StationDAO stationDAO;
+	
+	@Autowired
+	private RestaurantDAO restDAO;
 
 	@RequestMapping("/getStations")
-	@CrossOrigin(origins = "http://localhost:4200")
-	public List<Station> home(Model model) {
+	public List<Station> home() {
 		List<Station> allStations = stationDAO.getAllStations();
 		return allStations;
+	}
+
+	@RequestMapping("/station/{id}")
+	public List<Restaurant> getRestaurantsForStation(@PathVariable("id") int id) {
+		List<Restaurant> allRestaurants = restDAO.getAllRestaurantsForStation(id);
+		return allRestaurants;
 	}
 
 }
