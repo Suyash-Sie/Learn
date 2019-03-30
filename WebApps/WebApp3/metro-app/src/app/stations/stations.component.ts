3e882676af978988a59c3663970ac24c761e4950
suyash <suyash@192.168.1.10>
2019-03-31 00:00:24 +0530

    fixed disable reduce quantity button bug, implemented filtering by
    checkbox

--- WebApps/WebApp3/metro-app/src/app/stations/stations.component.ts
+++ WebApps/WebApp3/metro-app/src/app/stations/stations.component.ts
@@ -19,7 +19,9 @@ export class StationsComponent implements OnInit {
 	foodItems: Array<Food>;
 	quantityPerItem: Map<Number, Number>;
 	quantity : number = 0;
-	enableRemove: boolean = false;
+	disableRemove: Map<Number, boolean>;
+	restCheckbox: Map<Number, boolean>;
+	names: Array<Number>;
 	
 	constructor(private stationService: StationService,
 			private router: Router,
@@ -34,36 +36,55 @@ export class StationsComponent implements OnInit {
   
 	getRestaurantsForStation(filterVal: string) {
 		if(filterVal != "0") {
+			this.restCheckbox = new Map();
 			this.restService.getRestaurantsAtStation(filterVal).subscribe(data => {
 				this.restaurants = data;
-				let names = [];
+				this.names = [];
 				let i = 0;
 				this.quantity = 0;
 				for(var rest of data) {
-					names[i++] = rest.id;
+					this.names[i++] = rest.id;
+					this.restCheckbox.set(rest.id, true);
 				}
-				i = 0;
 				this.quantityPerItem = new Map();
-				this.foodService.getFoodItemsAtRestaurants(names).subscribe(food => {
-					for(var f of food) {
-						this.quantityPerItem.set(f.id, 0);
-					}
-					this.foodItems = food;
-				});
+				this.disableRemove = new Map();
+				this.populateFoodItems();
 			});
 		}
 	}
 	
+	populateFoodItems() {
+		this.foodService.getFoodItemsAtRestaurants(this.names).subscribe(food => {
+			for(var f of food) {
+				if(this.quantityPerItem.get(f.id) == null)
+					this.quantityPerItem.set(f.id, 0);
+				if(this.disableRemove.get(f.id) == null)
+					this.disableRemove.set(f.id, true);
+			}
+			this.foodItems = food;
+		});
+	}
+	
 	increaseQuantity(id: number, value: number){
-		this.quantityPerItem.set(id, value +1);
-		this.enableRemove = true;
+		this.quantityPerItem.set(id, value + 1);
+		this.disableRemove.set(id, false);
     }
 	
     decreaseQuantity(id: number, value: number){
 		this.quantityPerItem.set(id, value - 1);
-		if(value-1 == 0)
-			this.enableRemove = false;
+		if(value - 1 == 0)
+			this.disableRemove.set(id, true);
 		else
-			this.enableRemove = true;
+			this.disableRemove.set(id, false);
+    }
+    
+    onRestSelection(id: number, value: boolean) {
+    	this.restCheckbox.set(id, value);
+    	if(value == false) {
+	    	let index = this.names.indexOf(id);
+	    	this.names.splice(index, 1);
+    	} else
+    		this.names.push(id);
+    	this.populateFoodItems();
     }
 }
